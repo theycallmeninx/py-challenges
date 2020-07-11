de898c9e2f0ae81a239231a4333245adc828b960
Alex Boyle <alex.j.boyle@gmail.com>
2020-07-10 17:09:02 -0700

    Pr notes adding docstrings

--- persistence/persistence.py
+++ persistence/persistence.py
@@ -1,24 +1,39 @@
 #! python
 
+# converts the number to a string,
+# then converts the string to slices,
+# then converts the list of strings to ints
+splitter = lambda x: map(int, str(x)[:])
 
-def additive_persistence(n):
+
+def additive_persistence(n: int) -> None:
+    """ Takes a number and counts the number of split and add iterations it takes to reach a single digit.
+
+    Args:
+        n: the number to split/add/iterate
+    """
     final_count = 0
     while True:
         if len(str(n)) == 1:
             return final_count
         else:
-            n = sum(map(int, str(n)[:]))
+            n = sum(splitter(n))
             final_count += 1
 
 
-def multiplicative_persistence(n):
+def multiplicative_persistence(n: int) -> None:
+    """ Takes a number and counts the number of split and multiply iterations it takes to reach a single digit.
+
+    Args:
+        n: the number to split/multiply/iterate
+    """
     final_count = 0
     while True:
         if len(str(n)) == 1:
             return final_count
         else:
             _total = 1
-            for value in map(int, str(n)[:]):
+            for value in splitter(n):
                 _total *= value
             n = _total
             final_count += 1
